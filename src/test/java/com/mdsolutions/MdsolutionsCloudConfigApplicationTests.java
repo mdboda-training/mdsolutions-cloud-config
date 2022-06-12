5db2563938773e18ee867e1ab1e48a2554b2e09d
mdboda-training <boda.gadhilinga@gmail.com>
2022-06-12 20:03:02 +0530

    First Commit

---
+++ src/test/java/com/mdsolutions/MdsolutionsCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsolutionsCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
